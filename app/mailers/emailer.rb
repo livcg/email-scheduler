d4b9c07715931e81f68462fa4572a2e19a25e644
Liv Galendez <livcg@alum.mit.edu>
2015-01-22 00:04:23 +1100

    Set up sending emails through Mailgun

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -3,9 +3,10 @@ class Emailer < ApplicationMailer
  
   def schedule_email(email)
     @email = email
-    delivery_options = { user_name: ENV["EMAILERUSERNAME"],
-                         password: ENV["EMAILERPASSWORD"],
-                         address: ENV["EMAILERADDRESS"] }
+    delivery_options = { user_name: "postmaster@sandboxd4b0ebdc9fb14e6fa37eea18dd53bcad.mailgun.org",
+                         password: "21ff9cbbcf6afceaee1013649cc5cbd8",
+                         address: "smtp.mailgun.org",
+                         port: 587 }
     mail(to: @email.to, subject: @email.subject, body: @email.body,
          delivery_method_options: delivery_options)
   end
